5fe1bd090ffea7b0e9990cc540a13d963bba97dc
hyojeong <>
2025-02-07 00:14:30 +0900

    Initial commit

---
+++ src/pages/profile.svelte
@@ -0,0 +1,20 @@
+<script>
+    console.log("✅ profile.svelte 로드됨!");
+</script>
+
+<main>
+    <h1>Welcome to Profile Page</h1>
+    <p>This is the first screen of the app.</p>
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 2em;
+        font-family: Arial, sans-serif;
+    }
+
+    h1 {
+        color: #0070f3;
+    }
+</style>
